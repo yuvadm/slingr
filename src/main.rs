24b557f2616d54f75bfc2d6f076c50240180ada1
Yuval Adam <_@yuv.al>
2018-07-03 13:59:18 +0300

    Add basic clap structure

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+extern crate clap;
 extern crate colored;
 
 use colored::*;
+use clap::{Arg, App, SubCommand};
 
 fn main() {
-    println!("\n{}", "Rustcast v0.1.0".color("blue").bold());
-    println!("\n  usage: rustcast <media.file>\n");
+    let matches = App::new("Rustcast")
+        .version("0.1")
+        .author("Yuval Adam")
+        .about("A simple UPnP/DLNA casting player")
+        .arg(Arg::with_name("file")
+            .short("f")
+            .long("file")
+            .value_name("FILE")
+            .help("Media file to stream")
+            .required(true))
+        .get_matches();
+    //println!("\n{}", "Rustcast v0.1.0".color("blue").bold());
+    //println!("\n  usage: rustcast <media.file>\n");
 }
