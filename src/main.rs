db606609af91987f62e9496adc65ca74c02352d5
Yuval Adam <_@yuv.al>
2018-07-03 13:49:07 +0300

    Add basic usage print with color

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+extern crate colored;
+
+use colored::*;
+
 fn main() {
-    println!("Hello, world!");
+    println!("\n{}", "Rustcast v0.1.0".color("blue").bold());
+    println!("\n  usage: rustcast <media.file>\n");
 }
