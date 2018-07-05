a0537ab035ac31bb0f4f1f2107c9296c64cc4763
Yuval Adam <_@yuv.al>
2018-07-05 21:26:22 +0300

    Remove rustyline and cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
 extern crate clap;
 extern crate colored;
-extern crate rustyline;
 
 use colored::*;
 use clap::{Arg, App};
-// use rustyline::error::ReadlineError;
-// use rustyline::Editor;
 use std::path::Path;
 use std::process;
 
 fn main() {
-    let matches = App::new("Rustcast")
+    let app = App::new("Rustcast")
         .version("0.1")
         .author("Yuval Adam")
         .about("A simple UPnP/DLNA casting player")
@@ -18,9 +15,9 @@ fn main() {
             .value_name("FILE")
             .help("Media file to stream")
             .index(1)
-            .required(true))
-        .get_matches();
+            .required(true));
 
+    let matches = app.get_matches();
     let infile = matches.value_of("FILE").unwrap();
 
     if Path::new(infile).exists() {
